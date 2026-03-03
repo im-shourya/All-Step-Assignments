566c80fbbc3a2c00014cde2013846c340518bb1d
Shourya <parasharshourya07@gmail.com>
2026-03-03 21:36:36 +0530

    Level 2 Problem

---
+++ src/week1and2/level2/TriangleArea.java
@@ -0,0 +1,22 @@
+package week1and2.level2;
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the base of the triangle in cm: ");
+        double base = input.nextDouble();
+
+        System.out.print("Enter the height of the triangle in cm: ");
+        double height = input.nextDouble();
+
+        double areaCm = 0.5 * base * height;
+        double areaInches = areaCm / (2.54 * 2.54);
+
+        System.out.println("The Area of the triangle in sq in is " + areaInches
+                + " and sq cm is " + areaCm);
+
+        input.close();
+    }
+}
